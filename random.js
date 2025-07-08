ebea0dd1fa39d9c3dd389014af5dbda61428ea29
weifen nguyen <weifennguyen@gmail.com>
2025-07-08 16:36:09 +0700

    get list numbers that appear only once

--- random.js
+++ random.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 function generateUniqueRandomNumberFromFile(filePath, min, max) {
   const content = fs.readFileSync(filePath, 'utf8');
-  const existingNumbers = new Set(content.split('\n').map(n => Number(n.trim())));
+  const existingNumbers = content.split('\n').map(n => Number(n.trim())).filter(n => !isNaN(n));
 
   let randomNumber;
   let attempts = 0;
@@ -13,17 +13,26 @@ function generateUniqueRandomNumberFromFile(filePath, min, max) {
     if (attempts > 1000) {
       randomNumber = -1;
       for (let i = min; i <= max; i++) {
-        if (!existingNumbers.has(i)) {
+        if (existingNumbers.indexOf(i) == -1) {
           randomNumber = i;
           break;
         }
       }
       break;
     }
-  } while (existingNumbers.has(randomNumber));
+  } while (existingNumbers.indexOf(randomNumber) != -1);
 
   if (randomNumber == -1) {
-    console.log('No unique number in range');
+    console.log('No unique number in range!!!');
+    const numberList = existingNumbers;
+    const frequency = {};
+    for (const num of numberList) {
+      frequency[num] = (frequency[num] || 0) + 1;
+    }
+
+    const uniqueOnce = numberList.filter(num => frequency[num] === 1);
+
+    console.log('List numbers that appear only once:', uniqueOnce);
   } else {
     console.log('Generated unique number not in file:', randomNumber);
   }
